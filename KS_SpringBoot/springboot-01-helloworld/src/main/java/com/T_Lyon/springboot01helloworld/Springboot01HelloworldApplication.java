4245c149c57b59dd7f54e4eacc758a7556966d98
L_yon <1831069264@qq.com>
2021-05-31 20:13:16 +0800

    updata

--- KS_SpringBoot/springboot-01-helloworld/src/main/java/com/T_Lyon/springboot01helloworld/Springboot01HelloworldApplication.java
+++ KS_SpringBoot/springboot-01-helloworld/src/main/java/com/T_Lyon/springboot01helloworld/Springboot01HelloworldApplication.java
@@ -3,9 +3,10 @@ package com.T_Lyon.springboot01helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication ：标注这个类是一个springBoot的应用
 @SpringBootApplication
 public class Springboot01HelloworldApplication {
-
+    //将SpringBoot应用启动
     public static void main(String[] args) {
         SpringApplication.run(Springboot01HelloworldApplication.class, args);
     }
--- KS_SpringBoot/springboot-01-helloworld/src/main/java/com/T_Lyon/springboot01helloworld/controller/HelloController.java
+++ KS_SpringBoot/springboot-01-helloworld/src/main/java/com/T_Lyon/springboot01helloworld/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 //热部署
 
@@ -13,7 +14,12 @@ public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-
+        String q = "1";
+        int a = 123123;
+        String s = a + "";
+        System.out.println();
+        Integer integer = new Integer(q);
+        System.out.println(integer);
         return "hello";
     }
 }
