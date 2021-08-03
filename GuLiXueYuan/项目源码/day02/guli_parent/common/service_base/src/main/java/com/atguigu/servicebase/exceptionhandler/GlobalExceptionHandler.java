f6776e07727b2c1acc9d3cbf17dc2b8471086d89
L_yon <1831069264@qq.com>
2021-08-03 20:19:06 +0800

    update

--- GuLiXueYuan/项目源码/day02/guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ GuLiXueYuan/项目源码/day02/guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    //指定出现什么异常执行这个方法
+    //指定出现什么异常执行这个方法，此处所有异常都将执行这个异常类
     @ExceptionHandler(Exception.class)
     @ResponseBody //为了返回数据
     public R error(Exception e) {
