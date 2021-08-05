c3593540dca167d1fe341d7235ee04a72d58144a
L_yon <1831069264@qq.com>
2021-08-05 12:40:11 +0800

    update

--- GuLiXueYuan/项目源码/day07/后端代码/guli_parentyuanban/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ GuLiXueYuan/项目源码/day07/后端代码/guli_parentyuanban/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -3,14 +3,14 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduSubject;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.atguigu.eduservice.service.EduSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -36,6 +36,11 @@ public class EduSubjectController {
         subjectService.saveSubject(file,subjectService);
         return R.ok();
     }
-
+    @GetMapping("getAllSubject")
+    public R getAllSubject(){
+        //list集合泛型是一级分类
+        List<OneSubject> list = subjectService.getAllOneTwoSubject();
+        return R.ok().data("list",list);
+    }
 }
 
