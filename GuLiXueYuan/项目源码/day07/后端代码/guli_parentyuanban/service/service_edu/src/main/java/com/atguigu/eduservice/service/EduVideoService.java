9110f53752688cf74868830808e48e3b7a15d0ca
L_yon <1831069264@qq.com>
2021-08-05 14:40:43 +0800

    update

---
+++ GuLiXueYuan/项目源码/day07/后端代码/guli_parentyuanban/service/service_edu/src/main/java/com/atguigu/eduservice/service/EduVideoService.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduVideo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程视频 服务类
+ * </p>
+ *
+ * @author T_Lyon
+ * @since 2021-08-05
+ */
+public interface EduVideoService extends IService<EduVideo> {
+
+}
