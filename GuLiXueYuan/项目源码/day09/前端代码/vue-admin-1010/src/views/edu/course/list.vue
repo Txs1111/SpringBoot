959315bdd76610697ebc86e44a022fc074c8a1ec
L_yon <1831069264@qq.com>
2021-08-06 11:19:06 +0800

    update

--- GuLiXueYuan/项目源码/day09/前端代码/vue-admin-1010/src/views/edu/course/list.vue
+++ GuLiXueYuan/项目源码/day09/前端代码/vue-admin-1010/src/views/edu/course/list.vue
@@ -35,7 +35,7 @@
         </template>
       </el-table-column>
 
-      <el-table-column prop="title" label="课程名称" width="80" />
+      <el-table-column prop="title" label="课程名称"  />
 
       <el-table-column label="课程状态" width="80">
         <template slot-scope="scope">
@@ -43,7 +43,7 @@
         </template>
       </el-table-column>
 
-      <el-table-column prop="lessonNum" label="课时数" />
+      <el-table-column prop="lessonNum" label="课时数" width="200" />
 
       <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
 
@@ -51,13 +51,13 @@
 
       <el-table-column label="操作" width="200" align="center">
         <template slot-scope="scope">
-          <router-link :to="'/teacher/edit/'+scope.row.id">
-            <el-button type="primary" size="mini" icon="el-icon-edit">编辑课程基本信息</el-button>
+          <router-link :to="'/teacher/edit/'+scope.row.id" >
+            <el-button type="primary" size="mini" icon="el-icon-edit" style="margin:2px 0px" >编辑课程基本信息</el-button>
           </router-link>
           <router-link :to="'/teacher/edit/'+scope.row.id">
-            <el-button type="primary" size="mini" icon="el-icon-edit">编辑课程大纲息</el-button>
+            <el-button type="primary" size="mini" icon="el-icon-edit" style="margin:2px 0px">编辑课程大纲信息</el-button>
           </router-link>
-          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除课程信息</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)" style="margin:2px 0px">删除课程信息</el-button>
         </template>
       </el-table-column>
     </el-table>
