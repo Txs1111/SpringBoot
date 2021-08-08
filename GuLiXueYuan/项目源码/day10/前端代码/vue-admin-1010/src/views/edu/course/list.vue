84cecd54626dd9a6b04e6d0021b76188b0d152bb
L_yon <1831069264@qq.com>
2021-08-08 14:55:00 +0800

    update

--- GuLiXueYuan/项目源码/day10/前端代码/vue-admin-1010/src/views/edu/course/list.vue
+++ GuLiXueYuan/项目源码/day10/前端代码/vue-admin-1010/src/views/edu/course/list.vue
@@ -35,29 +35,29 @@
         </template>
       </el-table-column>
 
-      <el-table-column prop="title" label="课程名称" align="center" />
+      <el-table-column prop="title" label="课程名称" width="80" />
 
-      <el-table-column label="课程状态" width="150" align="center">
+      <el-table-column label="课程状态" width="80">
         <template slot-scope="scope">
           {{ scope.row.status==='Normal'?'已发布':'未发布' }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="lessonNum" label="课时数" width="100" align="center" />
+      <el-table-column prop="lessonNum" label="课时数" />
 
-      <el-table-column prop="gmtCreate" label="添加时间" width="200" align="center"/>
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
 
-      <el-table-column prop="viewCount" label="浏览数量" width="100" align="center" />
+      <el-table-column prop="viewCount" label="浏览数量" width="60" />
 
       <el-table-column label="操作" width="200" align="center">
         <template slot-scope="scope">
-          <router-link :to="'/teacher/edit/'+scope.row.id" >
-            <el-button type="primary" size="mini" icon="el-icon-edit" style="margin:2px 0px" >编辑课程基本信息</el-button>
+          <router-link :to="'/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">编辑课程基本信息</el-button>
           </router-link>
           <router-link :to="'/teacher/edit/'+scope.row.id">
-            <el-button type="primary" size="mini" icon="el-icon-edit" style="margin:2px 0px">编辑课程大纲信息</el-button>
+            <el-button type="primary" size="mini" icon="el-icon-edit">编辑课程大纲息</el-button>
           </router-link>
-          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)" style="margin:2px 0px">删除课程信息</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除课程信息</el-button>
         </template>
       </el-table-column>
     </el-table>
